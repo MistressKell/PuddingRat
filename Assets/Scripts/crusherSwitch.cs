fae88eb4381b6dc01eb81d890b9f95d9442cec75
Ronhas16 <132074896+Ronhas16@users.noreply.github.com>
2025-02-01 14:56:34 -0500

    Add files via upload

--- Assets/Scripts/crusherSwitch.cs
+++ Assets/Scripts/crusherSwitch.cs
@@ -5,17 +5,9 @@ public class crusherSwitch : MonoBehaviour
     public GameObject playerCrusher;
     public GameObject playerCrushed;
     private GameObject clone;
-    IEnumerator DestroyBox(GameObject box)
+    public void Crush()
     {
-        yield return new WaitForSeconds(.5f);
-        Destroy(box);
-    }
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
             clone = Instantiate(playerCrusher, new Vector3(playerCrushed.transform.position.x, (playerCrushed.transform.position.y + 5), playerCrushed.transform.position.z), Quaternion.identity);
-            DestroyBox(clone);
-        }
+            Destroy(clone, 3f);
     }
 }
