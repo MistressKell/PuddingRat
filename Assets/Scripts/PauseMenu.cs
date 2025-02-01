a0196eb25d1a5eae2787f22d075f2c36dbecf936
cjschrader <cadencejschrader@gmail.com>
2025-02-01 11:45:40 -0500

    Add files via upload

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -35,12 +35,16 @@ public class PauseMenu : MonoBehaviour
     {
         Time.timeScale = 0f;
         pauseMenu.SetActive(true) ;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void Resume()
     {
         Time.timeScale = 1f;
-        pauseMenu.SetActive(false) ;
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     public void Quit()
